309af9a508c13b8df2da974f5e170e5a561199c7
lGHOSTFACEl <120786298+lGHOSTFACEl@users.noreply.github.com>
2024-12-16 01:04:30 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "7663267275:AAFiZtvg-N-JcJAi1fxOIgLU493853SAAiA"  # токен ботика
+CHANNEL_URL = "https://t.me/+Ku2PKzgyQIw4ODAy"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002273685570"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002392986722"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/grabwin_support"
+ADMIN_ID = 981347434 # Ваш айди без скобочек
+PROMO = "1WGRAB" # промокод
